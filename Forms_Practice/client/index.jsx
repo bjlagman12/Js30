e9338d420fdcf4eb1d5727d6dd78f0bae3f9109d
Brian Lagman <brianlagman@Brians-MacBook-Pro.local>
2019-07-29 16:21:33 -0700

    Set up personal forms component

--- Forms_Practice/client/index.jsx
+++ Forms_Practice/client/index.jsx
@@ -1,20 +1,96 @@
 class App extends React.Component {
   constructor(){
     super()
+    this.state = {
+      firstname: '',
+      lastname: '',
+      address: '',
+      email: '',
+      phone: '',
+    };
+    this.changeInput = this.changeInput.bind(this);
+    this.nextForm = this.nextForm.bind(this)
   }
+  changeInput(input, value) {
+    this.setState({ [input]: value})
+  }
+  nextForm () {
+    console.log()
+  }
+  
   render(){
     return(
       <div>
-        <form>
-          <label>
-          First Name:
-          <input type="text" name="name" />
-          </label>
-          <input type="submit" value="submit"/>
-        </form>
+        <h2>
+          Checkout Here
+        </h2>
+        <PersonalForm changeInput={this.changeInput}/>
       </div>
     )
   }
+}
+
+
+const PersonalForm = ({ changeInput }) => {
+  return(
+      <form className="container">
+        <div>
+          <label>
+          First Name:
+          <input 
+            type="text" 
+            name="firstname" 
+            onChange={(e) => changeInput('firstname', e.target.value)} 
+          required/>
+          </label>
+        </div>
+        <div>
+          <label>
+          Last Name:
+          <input 
+            type="text" 
+            name="lastname" 
+            onChange={(e) => changeInput('lastname', e.target.value)} 
+          required/>
+          </label>
+        </div>
+        <div>
+          <label>
+          Address:
+          <input 
+            type="text" 
+            name="address" 
+            onChange={(e) => changeInput('address', e.target.value)} 
+          required/>
+          </label>
+        </div>
+        <div>
+          <label>
+          Email:
+          <input 
+            type="text" 
+            name="email" 
+            onChange={(e) => changeInput('email', e.target.value)} 
+          required/>
+          </label>
+        </div>
+        <div>
+          <label>
+          Phone Number
+          <input 
+            type="tel" 
+            name="phonenumber" 
+            maxLength="10" 
+            onChange={(e) => changeInput('phone', e.target.value)} 
+          required/>
+          </label>
+        </div>
+        <input 
+          type="submit" 
+          value="next"
+        />
+      </form>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- Forms_Practice/public/bundle.js
+++ Forms_Practice/public/bundle.js
@@ -1,18 +1,70 @@
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      firstname: '',
+      lastname: '',
+      address: '',
+      email: '',
+      phone: ''
+    };
+    this.changeInput = this.changeInput.bind(this);
+    this.nextForm = this.nextForm.bind(this);
+  }
+
+  changeInput(input, value) {
+    this.setState({
+      [input]: value
+    });
+  }
+
+  nextForm() {
+    console.log();
   }
 
   render() {
-    return React.createElement("div", null, React.createElement("form", null, React.createElement("label", null, "First Name:", React.createElement("input", {
-      type: "text",
-      name: "name"
-    })), React.createElement("input", {
-      type: "submit",
-      value: "submit"
-    })));
+    return React.createElement("div", null, React.createElement("h2", null, "Checkout Here"), React.createElement(PersonalForm, {
+      changeInput: this.changeInput
+    }));
   }
 
 }
+
+const PersonalForm = ({
+  changeInput
+}) => {
+  return React.createElement("form", {
+    className: "container"
+  }, React.createElement("div", null, React.createElement("label", null, "First Name:", React.createElement("input", {
+    type: "text",
+    name: "firstname",
+    onChange: e => changeInput('firstname', e.target.value),
+    required: true
+  }))), React.createElement("div", null, React.createElement("label", null, "Last Name:", React.createElement("input", {
+    type: "text",
+    name: "lastname",
+    onChange: e => changeInput('lastname', e.target.value),
+    required: true
+  }))), React.createElement("div", null, React.createElement("label", null, "Address:", React.createElement("input", {
+    type: "text",
+    name: "address",
+    onChange: e => changeInput('address', e.target.value),
+    required: true
+  }))), React.createElement("div", null, React.createElement("label", null, "Email:", React.createElement("input", {
+    type: "text",
+    name: "email",
+    onChange: e => changeInput('email', e.target.value),
+    required: true
+  }))), React.createElement("div", null, React.createElement("label", null, "Phone Number", React.createElement("input", {
+    type: "tel",
+    name: "phonenumber",
+    maxLength: "10",
+    onChange: e => changeInput('phone', e.target.value),
+    required: true
+  }))), React.createElement("input", {
+    type: "submit",
+    value: "next"
+  }));
+};
 
 ReactDOM.render(React.createElement(App, null), document.getElementById('app'));

e51faeeadaaf912064cbc8afd860218cce0154f6
Brian Lagman <brianlagman@Brians-MacBook-Pro.local>
2019-07-29 20:27:33 -0700

    Render inforamation on page

--- Forms_Practice/client/index.jsx
+++ Forms_Practice/client/index.jsx
@@ -2,38 +2,89 @@ class App extends React.Component {
   constructor(){
     super()
     this.state = {
+      page: 0,
       firstname: '',
       lastname: '',
-      address: '',
       email: '',
       phone: '',
+      address: '',
+      address2: '',
+      city: '',
+      state: '',
+      zip: ''
     };
-    this.changeInput = this.changeInput.bind(this);
+    this.changeInput = this.changeInput.bind(this)
     this.nextForm = this.nextForm.bind(this)
+    this.backForm = this.backForm.bind(this)
   }
   changeInput(input, value) {
     this.setState({ [input]: value})
   }
-  nextForm () {
-    console.log()
+
+  nextForm (e) {
+    console.log('test')
+    e.preventDefault()
+    let currentPage = this.state.page
+    if (currentPage === 0 || currentPage === 1 ) {
+      currentPage ++
+      this.setState({ page: currentPage })
+    }
+  }
+  backForm (e) {
+    e.preventDefault()
+    let currentPage = this.state.page
+    if (currentPage > 0) {
+      currentPage --
+      this.setState({ page: currentPage })
+    }
+  }
+  submit (e) {
+    e.preventDefault()
   }
   
   render(){
+    let page = this.state.page
+    let render = this.state
     return(
       <div>
         <h2>
           Checkout Here
         </h2>
-        <PersonalForm changeInput={this.changeInput}/>
+        {page === 0 && 
+          <PersonalForm 
+            changeInput={this.changeInput} 
+            nextForm={this.nextForm}/>
+        }
+         {page === 1 && 
+          <AddressInfo 
+            changeInput={this.changeInput} 
+            backForm={this.backForm}
+            nextForm={this.nextForm}/>
+        }
+        {page === 2 &&
+          <Final 
+            firstname={this.state.firstname}
+            lastname={this.state.lastname}
+            email={this.state.email}
+            phone={this.state.phone}
+            address={this.state.address}
+            address2={this.state.address2}
+            city={this.state.city}
+            state={this.state.state}
+            zip={this.state.zip}
+
+          />
+        }
       </div>
     )
   }
 }
 
-
-const PersonalForm = ({ changeInput }) => {
+const PersonalForm = ({ changeInput, nextForm }) => {
   return(
-      <form className="container">
+      <form 
+      onSubmit={nextForm}
+      className="container">
         <div>
           <label>
           First Name:
@@ -51,16 +102,6 @@ const PersonalForm = ({ changeInput }) => {
             type="text" 
             name="lastname" 
             onChange={(e) => changeInput('lastname', e.target.value)} 
-          required/>
-          </label>
-        </div>
-        <div>
-          <label>
-          Address:
-          <input 
-            type="text" 
-            name="address" 
-            onChange={(e) => changeInput('address', e.target.value)} 
           required/>
           </label>
         </div>
@@ -88,8 +129,159 @@ const PersonalForm = ({ changeInput }) => {
         <input 
           type="submit" 
           value="next"
-        />
+        /> 
       </form>
+  )
+}
+
+const AddressInfo = ({ changeInput, nextForm, backForm }) => {
+  return(
+      <form 
+        onSubmit={nextForm}
+        className="container">
+        <div>
+          <label>
+          Address:
+          <input 
+            type="text" 
+            name="Address" 
+            onChange={(e) => changeInput('address', e.target.value)} 
+          required/>
+          </label>
+        </div>
+        <div>
+          <label>
+          Address 2:
+          <input 
+            type="text" 
+            name="address2" 
+            onChange={(e) => changeInput('address2', e.target.value)} 
+          />
+          </label>
+        </div>
+        <div>
+          <label>
+          City:
+          <input 
+            type="text" 
+            name="city" 
+            onChange={(e) => changeInput('city', e.target.value)} 
+          required/>
+          </label>
+        </div>
+        <div>
+          <label>
+          State:
+          </label>
+          <select onChange={(e) => changeInput('state', e.target.value)} required>
+            <option value="AL">Alabama</option>
+            <option value="AK">Alaska</option>
+            <option value="AZ">Arizona</option>
+            <option value="AR">Arkansas</option>
+            <option value="CA">California</option>
+            <option value="CO">Colorado</option>
+            <option value="CT">Connecticut</option>
+            <option value="DE">Delaware</option>
+            <option value="DC">District Of Columbia</option>
+            <option value="FL">Florida</option>
+            <option value="GA">Georgia</option>
+            <option value="HI">Hawaii</option>
+            <option value="ID">Idaho</option>
+            <option value="IL">Illinois</option>
+            <option value="IN">Indiana</option>
+            <option value="IA">Iowa</option>
+            <option value="KS">Kansas</option>
+            <option value="KY">Kentucky</option>
+            <option value="LA">Louisiana</option>
+            <option value="ME">Maine</option>
+            <option value="MD">Maryland</option>
+            <option value="MA">Massachusetts</option>
+            <option value="MI">Michigan</option>
+            <option value="MN">Minnesota</option>
+            <option value="MS">Mississippi</option>
+            <option value="MO">Missouri</option>
+            <option value="MT">Montana</option>
+            <option value="NE">Nebraska</option>
+            <option value="NV">Nevada</option>
+            <option value="NH">New Hampshire</option>
+            <option value="NJ">New Jersey</option>
+            <option value="NM">New Mexico</option>
+            <option value="NY">New York</option>
+            <option value="NC">North Carolina</option>
+            <option value="ND">North Dakota</option>
+            <option value="OH">Ohio</option>
+            <option value="OK">Oklahoma</option>
+            <option value="OR">Oregon</option>
+            <option value="PA">Pennsylvania</option>
+            <option value="RI">Rhode Island</option>
+            <option value="SC">South Carolina</option>
+            <option value="SD">South Dakota</option>
+            <option value="TN">Tennessee</option>
+            <option value="TX">Texas</option>
+            <option value="UT">Utah</option>
+            <option value="VT">Vermont</option>
+            <option value="VA">Virginia</option>
+            <option value="WA">Washington</option>
+            <option value="WV">West Virginia</option>
+            <option value="WI">Wisconsin</option>
+            <option value="WY">Wyoming</option>
+          </select>
+        </div>
+        <div>
+          <label>
+          Zip Code:
+          <input 
+            type="number" 
+            name="zip" 
+            maxLength="5" 
+            onChange={(e) => changeInput('zip', e.target.value)} 
+          required/>
+          </label>
+        </div>
+        <button 
+          onClick={backForm}
+        >
+        back
+        </button> 
+        <input
+          type="submit" 
+          value="next"
+        /> 
+      </form>
+  )
+}
+
+const Final = ({firstname, lastname, email, phone, address, address2, city, state, zip}) => {
+  return(
+    <div>
+      <div>
+        {firstname}
+      </div>
+      <div>
+        {lastname}
+      </div>
+      <div>
+        {email}
+      </div>
+      <div>
+        {phone}
+      </div>
+      <div>
+        {address}
+      </div>
+      <div>
+        {address2}
+      </div>
+      <div>
+        {city}
+      </div>
+      <div>
+        {state}
+      </div>
+      <div>
+        {zip}
+      </div>
+    </div>
   )
 }
 
--- Forms_Practice/public/bundle.js
+++ Forms_Practice/public/bundle.js
@@ -2,14 +2,20 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      page: 0,
       firstname: '',
       lastname: '',
-      address: '',
       email: '',
-      phone: ''
+      phone: '',
+      address: '',
+      address2: '',
+      city: '',
+      state: '',
+      zip: ''
     };
     this.changeInput = this.changeInput.bind(this);
     this.nextForm = this.nextForm.bind(this);
+    this.backForm = this.backForm.bind(this);
   }
 
   changeInput(input, value) {
@@ -18,22 +24,66 @@ class App extends React.Component {
     });
   }
 
-  nextForm() {
-    console.log();
+  nextForm(e) {
+    console.log('test');
+    e.preventDefault();
+    let currentPage = this.state.page;
+
+    if (currentPage === 0 || currentPage === 1) {
+      currentPage++;
+      this.setState({
+        page: currentPage
+      });
+    }
+  }
+
+  backForm(e) {
+    e.preventDefault();
+    let currentPage = this.state.page;
+
+    if (currentPage > 0) {
+      currentPage--;
+      this.setState({
+        page: currentPage
+      });
+    }
+  }
+
+  submit(e) {
+    e.preventDefault();
   }
 
   render() {
-    return React.createElement("div", null, React.createElement("h2", null, "Checkout Here"), React.createElement(PersonalForm, {
-      changeInput: this.changeInput
+    let page = this.state.page;
+    let render = this.state;
+    return React.createElement("div", null, React.createElement("h2", null, "Checkout Here"), page === 0 && React.createElement(PersonalForm, {
+      changeInput: this.changeInput,
+      nextForm: this.nextForm
+    }), page === 1 && React.createElement(AddressInfo, {
+      changeInput: this.changeInput,
+      backForm: this.backForm,
+      nextForm: this.nextForm
+    }), page === 2 && React.createElement(Final, {
+      firstname: this.state.firstname,
+      lastname: this.state.lastname,
+      email: this.state.email,
+      phone: this.state.phone,
+      address: this.state.address,
+      address2: this.state.address2,
+      city: this.state.city,
+      state: this.state.state,
+      zip: this.state.zip
     }));
   }
 
 }
 
 const PersonalForm = ({
-  changeInput
+  changeInput,
+  nextForm
 }) => {
   return React.createElement("form", {
+    onSubmit: nextForm,
     className: "container"
   }, React.createElement("div", null, React.createElement("label", null, "First Name:", React.createElement("input", {
     type: "text",
@@ -44,11 +94,6 @@ const PersonalForm = ({
     type: "text",
     name: "lastname",
     onChange: e => changeInput('lastname', e.target.value),
-    required: true
-  }))), React.createElement("div", null, React.createElement("label", null, "Address:", React.createElement("input", {
-    type: "text",
-    name: "address",
-    onChange: e => changeInput('address', e.target.value),
     required: true
   }))), React.createElement("div", null, React.createElement("label", null, "Email:", React.createElement("input", {
     type: "text",
@@ -65,6 +110,161 @@ const PersonalForm = ({
     type: "submit",
     value: "next"
   }));
+};
+
+const AddressInfo = ({
+  changeInput,
+  nextForm,
+  backForm
+}) => {
+  return React.createElement("form", {
+    onSubmit: nextForm,
+    className: "container"
+  }, React.createElement("div", null, React.createElement("label", null, "Address:", React.createElement("input", {
+    type: "text",
+    name: "Address",
+    onChange: e => changeInput('address', e.target.value),
+    required: true
+  }))), React.createElement("div", null, React.createElement("label", null, "Address 2:", React.createElement("input", {
+    type: "text",
+    name: "address2",
+    onChange: e => changeInput('address2', e.target.value)
+  }))), React.createElement("div", null, React.createElement("label", null, "City:", React.createElement("input", {
+    type: "text",
+    name: "city",
+    onChange: e => changeInput('city', e.target.value),
+    required: true
+  }))), React.createElement("div", null, React.createElement("label", null, "State:"), React.createElement("select", {
+    onChange: e => changeInput('state', e.target.value),
+    required: true
+  }, React.createElement("option", {
+    value: "AL"
+  }, "Alabama"), React.createElement("option", {
+    value: "AK"
+  }, "Alaska"), React.createElement("option", {
+    value: "AZ"
+  }, "Arizona"), React.createElement("option", {
+    value: "AR"
+  }, "Arkansas"), React.createElement("option", {
+    value: "CA"
+  }, "California"), React.createElement("option", {
+    value: "CO"
+  }, "Colorado"), React.createElement("option", {
+    value: "CT"
+  }, "Connecticut"), React.createElement("option", {
+    value: "DE"
+  }, "Delaware"), React.createElement("option", {
+    value: "DC"
+  }, "District Of Columbia"), React.createElement("option", {
+    value: "FL"
+  }, "Florida"), React.createElement("option", {
+    value: "GA"
+  }, "Georgia"), React.createElement("option", {
+    value: "HI"
+  }, "Hawaii"), React.createElement("option", {
+    value: "ID"
+  }, "Idaho"), React.createElement("option", {
+    value: "IL"
+  }, "Illinois"), React.createElement("option", {
+    value: "IN"
+  }, "Indiana"), React.createElement("option", {
+    value: "IA"
+  }, "Iowa"), React.createElement("option", {
+    value: "KS"
+  }, "Kansas"), React.createElement("option", {
+    value: "KY"
+  }, "Kentucky"), React.createElement("option", {
+    value: "LA"
+  }, "Louisiana"), React.createElement("option", {
+    value: "ME"
+  }, "Maine"), React.createElement("option", {
+    value: "MD"
+  }, "Maryland"), React.createElement("option", {
+    value: "MA"
+  }, "Massachusetts"), React.createElement("option", {
+    value: "MI"
+  }, "Michigan"), React.createElement("option", {
+    value: "MN"
+  }, "Minnesota"), React.createElement("option", {
+    value: "MS"
+  }, "Mississippi"), React.createElement("option", {
+    value: "MO"
+  }, "Missouri"), React.createElement("option", {
+    value: "MT"
+  }, "Montana"), React.createElement("option", {
+    value: "NE"
+  }, "Nebraska"), React.createElement("option", {
+    value: "NV"
+  }, "Nevada"), React.createElement("option", {
+    value: "NH"
+  }, "New Hampshire"), React.createElement("option", {
+    value: "NJ"
+  }, "New Jersey"), React.createElement("option", {
+    value: "NM"
+  }, "New Mexico"), React.createElement("option", {
+    value: "NY"
+  }, "New York"), React.createElement("option", {
+    value: "NC"
+  }, "North Carolina"), React.createElement("option", {
+    value: "ND"
+  }, "North Dakota"), React.createElement("option", {
+    value: "OH"
+  }, "Ohio"), React.createElement("option", {
+    value: "OK"
+  }, "Oklahoma"), React.createElement("option", {
+    value: "OR"
+  }, "Oregon"), React.createElement("option", {
+    value: "PA"
+  }, "Pennsylvania"), React.createElement("option", {
+    value: "RI"
+  }, "Rhode Island"), React.createElement("option", {
+    value: "SC"
+  }, "South Carolina"), React.createElement("option", {
+    value: "SD"
+  }, "South Dakota"), React.createElement("option", {
+    value: "TN"
+  }, "Tennessee"), React.createElement("option", {
+    value: "TX"
+  }, "Texas"), React.createElement("option", {
+    value: "UT"
+  }, "Utah"), React.createElement("option", {
+    value: "VT"
+  }, "Vermont"), React.createElement("option", {
+    value: "VA"
+  }, "Virginia"), React.createElement("option", {
+    value: "WA"
+  }, "Washington"), React.createElement("option", {
+    value: "WV"
+  }, "West Virginia"), React.createElement("option", {
+    value: "WI"
+  }, "Wisconsin"), React.createElement("option", {
+    value: "WY"
+  }, "Wyoming"))), React.createElement("div", null, React.createElement("label", null, "Zip Code:", React.createElement("input", {
+    type: "number",
+    name: "zip",
+    maxLength: "5",
+    onChange: e => changeInput('zip', e.target.value),
+    required: true
+  }))), React.createElement("button", {
+    onClick: backForm
+  }, "back"), React.createElement("input", {
+    type: "submit",
+    value: "next"
+  }));
+};
+
+const Final = ({
+  firstname,
+  lastname,
+  email,
+  phone,
+  address,
+  address2,
+  city,
+  state,
+  zip
+}) => {
+  return React.createElement("div", null, React.createElement("div", null, firstname), React.createElement("div", null, lastname), React.createElement("div", null, email), React.createElement("div", null, phone), React.createElement("div", null, address), React.createElement("div", null, address2), React.createElement("div", null, city), React.createElement("div", null, state), React.createElement("div", null, zip));
 };
 
 ReactDOM.render(React.createElement(App, null), document.getElementById('app'));

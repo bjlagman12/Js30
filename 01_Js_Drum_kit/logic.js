17058406081e983f00c4215e6a5ec46100304aa8
Brian Lagman <brianlagman@Brians-MacBook-Pro.local>
2019-08-08 20:56:36 -0700

    Set up 808 drumkut

--- 01_Js_Drum_kit/logic.js
+++ 01_Js_Drum_kit/logic.js
@@ -5,8 +5,8 @@ console.log(e.keyCode)
   let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
   audio.currentTime = 0
   audio.play()
-  // console.log(document.querySelector('audio[data-key="65"]').play())
+
 }
 
 window.addEventListener('keydown', playSound)
-// document.getElementsByClassName('sound')[0].addEventListener('click', playSound)
+

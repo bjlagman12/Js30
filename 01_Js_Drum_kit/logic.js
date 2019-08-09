f37557979e54742c97dd56460df536a636aa615f
Brian Lagman <brianlagman@Brians-MacBook-Pro.local>
2019-08-08 23:09:06 -0700

    Update styles

--- 01_Js_Drum_kit/logic.js
+++ 01_Js_Drum_kit/logic.js
@@ -1,12 +1,10 @@
-console.log('initial test')
+console.log("initial test");
 
-let playSound = (e) => {
-console.log(e.keyCode)  
-  let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-  audio.currentTime = 0
-  audio.play()
+let playSound = e => {
+  console.log(e.keyCode);
+  let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  audio.currentTime = 0;
+  audio.play();
+};
 
-}
-
-window.addEventListener('keydown', playSound)
-
+window.addEventListener("keydown", playSound);
--- Practice_FE/calc.js
+++
@@ -1,7 +0,0 @@
-var calc = (num) => {
-  // var num = ''
-  // document.getElementById("num").innerHTML = num
-  
-  // console.log('hi')
-  console.log(num)
-}

74f96c8374e3b4c91012105c31d981c79c3a40df
Brian Lagman <brianlagman@Brians-MacBook-Pro.local>
2019-08-09 00:35:40 -0700

    Complete Button Transition

--- 01_Js_Drum_kit/logic.js
+++ 01_Js_Drum_kit/logic.js
@@ -1,10 +1,23 @@
 console.log("initial test");
 
+let removeTransition = (e) => {
+  e.target.classList.remove('play')
+}
+
 let playSound = e => {
-  console.log(e.keyCode);
   let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  let key = document.querySelector(`div[data-key="${e.keyCode}"]`)
+
+  if (!audio) return;
+  
+  key.classList.add('play')
   audio.currentTime = 0;
   audio.play();
 };
 
+  let keys = Array.from(document.querySelectorAll('.key'))
+  for (var i = 0; i < keys.length; i++) {
+    var each = keys[i]
+    each.addEventListener('transitionend', removeTransition)
+  }
 window.addEventListener("keydown", playSound);

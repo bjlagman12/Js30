b3264299cbf6610b72ef1c10cb93bb4dcd024171
Brian Lagman <brianlagman@Brians-MacBook-Pro.local>
2019-08-19 13:00:29 -0700

    Complete CSS variables

--- 03_CSS Variables/app.js
+++ 03_CSS Variables/app.js
@@ -1,1 +1,16 @@
 console.log('Initialize App')
+
+const inputs = document.querySelectorAll('.controls input');
+
+let handleUpdate = (e) => {
+  let change = e.target.value;
+  let unit = e.target.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${e.target.name}`, change + unit)
+
+
+}
+
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
